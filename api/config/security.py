322e80fadb48cdd1217ec4027e35dc3041ab2952
andresbonelli <andresbonellipiano@gmail.com>
2024-11-29 09:31:58 -0300

    update gitignore. update security allowed hosts

--- api/config/security.py
+++ api/config/security.py
@@ -9,12 +9,8 @@ refresh_token_exp = timedelta(days=1)
 
 allowed_origins = [
     "*",
-    f"http://{HOST_URL}",
-    f"http://{HOST_URL}:{HOST_PORT}",
-    f"http://{FRONTEND_HOST}",
-    f"http://{FRONTEND_HOST}:{HOST_PORT}",
-    f"https://{HOST_URL}",
-    f"https://{HOST_URL}:{HOST_PORT}",
-    f"https://{FRONTEND_HOST}",
-    f"https://{FRONTEND_HOST}:{HOST_PORT}",
+    HOST_URL,
+    f"{HOST_URL}:{HOST_PORT}",
+    FRONTEND_HOST,
+    f"{FRONTEND_HOST}:{HOST_PORT}",
 ]
