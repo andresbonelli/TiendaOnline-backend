12e06281bd7df2ea736185e7bb9a18858e0348b5
andresbonelli <andresbonellipiano@gmail.com>
2025-05-31 14:24:38 -0300

    change envs read

--- api/config/__base.py
+++ api/config/__base.py
@@ -28,31 +28,31 @@ load_dotenv()
 
 APP_TITLE = "Devlights Bootcamp 3.0 - Proyecto Final"
 
-API_ENV = os.getenv("API_ENV")
+API_ENV = os.environ.get("API_ENV")
 
-MONGODB_URI = os.getenv("MONGODB_CONNECTION_STRING")
+MONGODB_URI = os.environ.get("MONGODB_CONNECTION_STRING")
 if not MONGODB_URI:
     raise Exception("MongoDB connection string not found")
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 if not SECRET_KEY:
     raise Exception("Secret key not found")
 
-REFRESH_KEY = os.getenv("REFRESH_KEY")
+REFRESH_KEY = os.environ.get("REFRESH_KEY")
 if not REFRESH_KEY:
     raise Exception("Refresh token secret key not found")
 
-HOST_URL = os.getenv("HOST_URL", "127.0.0.1")
-PUBLIC_HOST_URL = os.getenv("PUBLIC_HOST_URL", "127.0.0.1")
-FRONTEND_HOST = os.getenv("FRONTEND_HOST", "localhost")
-FRONTEND_PORT = int(os.getenv("FRONTEND_PORT", "3000"))
-HOST_PORT = int(os.getenv("HOST_PORT", "8000"))
-MAIL_USERNAME = os.getenv("MAIL_USERNAME", "")
-MAIL_PASSWORD = os.getenv("MAIL_PASSWORD", "")
-MAIL_FROM = os.getenv("MAIL_FROM", "")
-MAIL_PORT = int(os.getenv("MAIL_PORT", "1025"))
-MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp")
-RESEND_API_KEY = os.getenv("RESEND_API_KEY", "")
+HOST_URL = os.environ.get("HOST_URL", "127.0.0.1")
+PUBLIC_HOST_URL = os.environ.get("PUBLIC_HOST_URL", "127.0.0.1")
+FRONTEND_HOST = os.environ.get("FRONTEND_HOST", "localhost")
+FRONTEND_PORT = int(os.environ.get("FRONTEND_PORT", "3000"))
+HOST_PORT = int(os.environ.get("HOST_PORT", "8000"))
+MAIL_USERNAME = os.environ.get("MAIL_USERNAME", "")
+MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD", "")
+MAIL_FROM = os.environ.get("MAIL_FROM", "")
+MAIL_PORT = int(os.environ.get("MAIL_PORT", "1025"))
+MAIL_SERVER = os.environ.get("MAIL_SERVER", "smtp")
+RESEND_API_KEY = os.environ.get("RESEND_API_KEY", "")
 
 
 logger = logging.getLogger("uvicorn")
